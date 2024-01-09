5f00de129d193b6f5e448c1583c1f02252bba77d
DanieleRuiu <daniele1930@gmail.com>
2024-01-09 18:02:28 +0100

    Esercizi giorno 3

--- src/it/epicode/week1/day2/Esercizio4.java
+++ src/it/epicode/week1/day2/Esercizio4.java
@@ -6,8 +6,10 @@ import java.util.Scanner;
 public class Esercizio4 {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System out.println("Scrivi un numero");
+    System.out.println("Scrivi un numero");
     int numero = scanner.nextInt();
+
+
 
     for (int i=numero; i>0; i--) {
         System.out.println(i);
