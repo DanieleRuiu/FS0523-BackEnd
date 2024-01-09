73c86d3356e7e7dcac1f3efcd082190fee99ddd8
DanieleRuiu <daniele1930@gmail.com>
2024-01-09 17:54:46 +0100

    Esercizi giorno2

--- src/it/epicode/week1/day2/Esercizio4.java
+++ src/it/epicode/week1/day2/Esercizio4.java
@@ -1,36 +1,16 @@
 package it.epicode.week1.day2;
 
 import java.awt.*;
+import java.util.Scanner;
 
 public class Esercizio4 {
     public static void main(String[] args) {
-        jframe jframe = new jframe();
-        jlabel jlabel = new jlabel();
-        jframe.setLayout(new FlowLayout());
-        jframe.setBounds(500, 300, 400, 100);
+    Scanner scanner = new Scanner(System.in);
+    System out.println("Scrivi un numero");
+    int numero = scanner.nextInt();
 
-        jframe.add(jlabel);
-        jframe.setVisible(true);
-
-        timer timer = new timer();
-        timer.scheduleAtFixedRate(new timerTask() {
-            int i = 20;
-
-            pubblic
-
-            void run() {
-                jlabel.setText("Secondi rimanenti: " + i);
-                i--;
-
-                if (i < 0) {
-                    timer.cancel();
-                    jlabel.setText("Tempo scaduto");
-
-                }
-            }
-        }, 0, 1000);
-
-
+    for (int i=numero; i>0; i--) {
+        System.out.println(i);
     }
-
+    }
 }
